143e84e2c139f74aaad8f202b2e69ee209d43e8b
Sergio Gonzalez <sergiogonzalezlopez101@gmail.com>
2023-06-07 19:01:50 -0600

    Generated files are deleted

--- index.js
+++ index.js
@@ -1,27 +1,44 @@
 const express = require('express');
 const { exec } = require('child_process');
 const app = express();
-
-const sourceDiretory = './java/src/';
-const outputDirectory = './java';
+const fs = require('fs');
 
 app.get('/generate', (req, res) => {
-  exec(`java -jar ${outputDirectory}/java2plantuml.jar ${sourceDiretory}`, (err, stdout, stderr) => { // > ${outputDirectory}output_${uniqueId}.puml
+  exec(`cd java/ && java -jar java2plantuml.jar src/`, (err, stdout, stderr) => {
     if (err) {
       console.error(err);
       return res.status(500).send('Error al ejecutar el archivo jar.');
     }
 
-    exec(`java -jar ${outputDirectory}/plantuml-1.2023.8.jar output.puml`, (err, stdout, stderr) => {
+    exec(`cd java/ && java -jar plantuml-1.2023.8.jar output.puml`, (err, stdout, stderr) => {
       if (err) {
         console.error(err);
         return res.status(500).send('Error al generar la imagen UML.');
       }
-      res.download(`output.png`);
-    });
+      res.download(`./java/output.png`, (err) => {
+        if (err) {
+          console.error('Hubo un error al descargar el archivo:', err);
+        }
 
+        fs.unlink('./java/output.puml', (err) => {
+          if (err) {
+            console.error('Hubo un error al intentar eliminar el archivo:', err);
+          } else {
+            console.log('Archivo UML eliminado exitosamente');
+          }
+        });
+
+        fs.unlink('./java/output.png', (err) => {
+          if (err) {
+            console.error('Hubo un error al intentar eliminar el archivo:', err);
+          } else {
+            console.log('Archivo IMG eliminado exitosamente');
+          }
+        });
+      });
+    });
   });
-});   
+});
 
 app.listen(3000, () => {
   console.log('Server is running . . .');
